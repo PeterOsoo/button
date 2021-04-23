4c39b85d1872db7f2343e673258d3285237ea29b
peterosoo <posoo617@gmail.com>
2021-04-23 11:43:19 +0300

    log to console

---
+++ HitchHikerButton.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("HitchhikerButton")
+
+// console.log(btn)
+
+btn.addEventListener("click", e => {
+	console.log("Please do not press this button again")
+	console.log("Do not press this button again.")
+	console.log("DO NOT PRESS THIS BUTTON AGAIN!")
+})
